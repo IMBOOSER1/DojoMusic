14b9b45f1d23795d1b7ce48f49db12846d76867f
IMBOOSER1 <44923714+IMBOOSER1@users.noreply.github.com>
2018-11-30 16:39:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,152 @@ client.on('message', msg => {
 
 
 
+	client.on('message', message =>{
+    if(message.content === '-ping'){
+let start = Date.now(); message.channel.send('pong').then(message => { 
+message.edit(`\`\`\`js
+Time taken: ${Date.now() - start} ms
+Discord API: ${client.ping.toFixed(0)} ms\`\`\``);
+    });
+    }
+});
+
+
+
+
+
+
+
+
+
+	const HeRo = new Discord.Client();
+client.on('message', message => {
+var prefix = "-";
+
+    if (message.content === prefix + "date") {
+        if (!message.channel.guild) return message.reply('** This command only for servers **');  
+        var currentTime = new Date(),
+            Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate();
+
+            var Date15= new Discord.RichEmbed()
+            .setTitle("**「  Date  」 **")
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setDescription( "「"+ Day + "-" + Month + "-" + Year + "」")
+             message.channel.sendEmbed(Date15);
+    }
+});
+
+
+
+
+
+
+
+	client.on('message', Sal => { // By Salto7#4595
+  if(Sal.content === '-bot') { //هنا تغير البرفيكس
+  var embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setThumbnail(client.user.avatarURL)
+  .addField('Bot Name', client.user.username, true)
+  .setFooter(client.user.tag, client.user.avatarURL, true)
+  .addField('Bot Tag', client.user.discriminator, true)
+  .addField('Bot id', client.user.id, true)
+  .addField('Create Bot At', client.user.createdAt, true)
+  Sal.channel.sendEmbed(embed);
+}
+});
+
+
+
+
+
+
+
+
+	client.on('message', message => {
+    if (message.content.startsWith("-avatar")) {
+        if (message.author.bot) return
+        var mentionned = message.mentions.users.first();
+    var omar;
+      if(mentionned){
+          var omar = mentionned;
+      } else {
+          var omar = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor('Avatar Link :')
+        .setTitle('Click Here')
+        .setURL(`${omar.avatarURL}`)
+        .setImage(`${omar.avatarURL}`)
+        .setFooter('name bot',client.user.avatarURL) 
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Dojo-Bot`,"https://www.twitch.tv/settings/profile")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
