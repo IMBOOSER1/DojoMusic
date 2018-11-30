99bab2dc94df045323a6b87d94af9555c07fcfc1
IMBOOSER1 <44923714+IMBOOSER1@users.noreply.github.com>
2018-11-30 15:23:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+client.login('NTE1MTUyMzE2MDY0NTk1OTg3.DuK1xw.fM_-KcAft3BDIL5dY3-yByDLPB0');
